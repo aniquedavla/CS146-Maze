120a07788901781384c44cfb88ea006e0d291f95
Keshav Narula <narulakeshav13@gmail.com>
2016-11-29 19:18:58 -0800

    Put Vertex class in package

---
+++ src/sjsu/narula/cs146/project3/Vertex.java
@@ -0,0 +1,11 @@
+package sjsu.narula.cs146.project3;
+
+public class Vertex {
+    public char label; // label (e.g. ‘A’)
+    public boolean wasVisited;
+
+    public Vertex(char lab) { // constructor
+        label = lab;
+        wasVisited = false;
+    }
+} // end class Vertex
