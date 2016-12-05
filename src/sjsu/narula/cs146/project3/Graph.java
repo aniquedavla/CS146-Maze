f6548c47a71e424fc035ba0bb2f5e8b6c2a8763d
rodion13 <ryaryy@gmail.com>
2016-12-04 16:08:01 -0800

    Creating a prefect maze from a graph

--- src/sjsu/narula/cs146/project3/Graph.java
+++ src/sjsu/narula/cs146/project3/Graph.java
@@ -11,24 +11,25 @@ public class Graph {
   private Vertex vertexList[];
   private Vertex adjMatrix[][];
   private int numberOfVertices;
+  private int adjMatrixSize;
 
   /**
    * Constructs the Maze
    * @param dimension dimensions of the maze
    */
   public Graph(int dimension) {
-    randomGen = new java.util.Random(0);
+    randomGen = new Random();
 
     vertexList = new Vertex[dimension * dimension];
     adjMatrix = new Vertex[dimension][dimension];
     numberOfVertices = 0;
+    adjMatrixSize = adjMatrix.length - 1;
 
     for(int j=0; j < dimension; j++) // set adjacency
       for(int k=0; k < dimension; k++) // matrix to 0
         adjMatrix[j][k] = new Vertex(j, k);
 
-    adjMatrix[0][0].UP = false;
-    adjMatrix[dimension-1][dimension-1].DOWN = false;
+    createMaze();
   }
 
 
@@ -47,27 +48,49 @@ public class Graph {
    * Creates the maze
    */
   public void createMaze() {
+    Stack<Vertex> vertexStack = new Stack<Vertex>();
+    Vertex currentVertex = adjMatrix[0][0];
+    int visitedVertices = 1;
+    int totalVertices = vertexList.length;
+    boolean lastVertex = false;
 
+    while (visitedVertices < totalVertices) {
+      List<Vertex> neighborhoodList = findNeighbors(currentVertex);
+      Vertex selectedVertex = currentVertex;
+
+      if(neighborhoodList.size() > 0 ){
+        vertexStack.push(currentVertex);
+        int randomValue = (int) (myRandom() * neighborhoodList.size());
+        currentVertex = neighborhoodList.get(randomValue);
+        breakWalls(currentVertex, selectedVertex);
+        visitedVertices++;
+      }
+      else {
+        currentVertex = vertexStack.pop();
+      }
+    }
+
+    adjMatrix[0][0].UP = false;
+    adjMatrix[adjMatrixSize][adjMatrixSize].DOWN = false;
   }
 
   /**
    * Prints out the maze with ASCII characters
-   *
-   * @return maze
+   * @return String representation of maze
    */
   public String printMaze() {
-    String maze = "+";
+    String maze = "";
     int size = adjMatrix.length;
 
     for (int i = 0; i < size; i++){
-      maze += (!adjMatrix[i][i].UP) ? " " : "+-";
+      maze += (i == 0) ? "+ " : "+-";
     }
     maze += "+\n";
 
     for (int i = 0; i < size; i++){
       maze += "|";
       for (int j = 0; j < size - 1; j++){
-        maze += (adjMatrix[j][i].LEFT) ? " |" : "  ";
+        maze += (adjMatrix[j][i].RIGHT) ? " |" : "  ";
       }
      maze += " |\n+";
      if (i < size - 1) {
@@ -86,6 +109,62 @@ public class Graph {
 
     return maze;
   }
+
+  /**
+   * Find all Vertices connected to the givenVertex
+   * @param givenVertex
+   * @return
+   */
+  public List<Vertex> findNeighbors(Vertex givenVertex) {
+    List<Vertex> neighborhoodList = new ArrayList<Vertex>();
+    int matrixSize = adjMatrixSize;
+
+    //Checking all Vertices in the Neighborhood and adding them to the list only if they are full.
+    if (givenVertex.j < matrixSize && checkFullNeighborhood(adjMatrix[givenVertex.i][givenVertex.j + 1]))
+      neighborhoodList.add(adjMatrix[givenVertex.i][givenVertex.j + 1]);
+
+    if (givenVertex.i < matrixSize && checkFullNeighborhood(adjMatrix[givenVertex.i + 1][givenVertex.j]))
+      neighborhoodList.add((adjMatrix[givenVertex.i + 1][givenVertex.j]));
+
+    if (givenVertex.j > 0 && checkFullNeighborhood(adjMatrix[givenVertex.i][givenVertex.j - 1]))
+      neighborhoodList.add((adjMatrix[givenVertex.i][givenVertex.j - 1]));
+
+    if (givenVertex.i > 0 && checkFullNeighborhood(adjMatrix[givenVertex.i - 1][givenVertex.j]))
+      neighborhoodList.add((adjMatrix[givenVertex.i - 1][givenVertex.j]));
+
+    return neighborhoodList;
+  }
+
+
+  public boolean checkFullNeighborhood(Vertex givenVertex) {
+    return givenVertex.UP && givenVertex.DOWN && givenVertex.LEFT && givenVertex.RIGHT;
+  }
+
+  public void breakWalls(Vertex v1, Vertex v2) {
+    int matrixSize = adjMatrixSize;
+
+    if (v1.i < matrixSize && adjMatrix[v1.i + 1][v1.j].equals(v2)){
+      v1.RIGHT = false;
+      v2.LEFT = false;
+    }
+
+    if (v1.j < matrixSize && adjMatrix[v1.i][v1.j + 1].equals(v2)){
+      v1.DOWN = false;
+      v2.UP = false;
+    }
+
+    if (v1.i > 0 && adjMatrix[v1.i - 1][v1.j].equals(v2)) {
+      v1.LEFT = false;
+      v2.RIGHT = false;
+    }
+
+    if (v1.j > 0 && adjMatrix[v1.i][v1.j - 1].equals(v2)){
+      v1.UP = false;
+      v2.DOWN = false;
+    }
+  }
+
+
 
   /**
    * Prints the Breadth-first Search maze
--- src/sjsu/narula/cs146/project3/Tester.java
+++ src/sjsu/narula/cs146/project3/Tester.java
@@ -6,7 +6,12 @@ package src.sjsu.narula.cs146.project3;
 public class Tester {
   public static void main(String args[])
   {
-    Graph g = new Graph(5);
+    Graph g = new Graph(2);
     System.out.println(g.printMaze());
+    Graph g2 = new Graph(2);
+    System.out.println(g2.printMaze());
+
+    //g.createMaze();
+    //System.out.print(g.printMaze());
   }
 }
