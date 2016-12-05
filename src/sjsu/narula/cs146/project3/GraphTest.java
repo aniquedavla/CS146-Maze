a3a647e75df2a53d2b316eaaad7c3298eea9aff4
Keshav Narula <narulakeshav13@gmail.com>
2016-12-04 20:58:29 -0800

    Document and fix spaces

--- src/sjsu/narula/cs146/project3/GraphTest.java
+++ src/sjsu/narula/cs146/project3/GraphTest.java
@@ -8,27 +8,27 @@ import static org.junit.Assert.*;
 public class GraphTest {
 
     @org.junit.Test
-    public void graph4x4() {
+    public void graph1x1() {
         System.out.println("\n\n------- 4x4 Graph -------");
-        Graph graph = new Graph(3);
+        Graph graph = new Graph(1);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
     }
 
-    /*
     @org.junit.Test
-    public void graph8x8() {
-        System.out.println("\n\n------- 8x8 Graph -------");
-        Graph graph = new Graph(8);
+    public void graph5x5() {
+        System.out.println("\n\n------- 4x4 Graph -------");
+        Graph graph = new Graph(5);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
     }
+
 
     @org.junit.Test
     public void graph10x10() {
-        System.out.println("\n\n------ 10x10 Graph ------");
+        System.out.println("\n\n------- 8x8 Graph -------");
         Graph graph = new Graph(10);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
@@ -36,9 +36,9 @@ public class GraphTest {
     }
 
     @org.junit.Test
-    public void graph16x16() {
-        System.out.println("\n\n------ 16x16 Graph ------");
-        Graph graph = new Graph(16);
+    public void graph15x15() {
+        System.out.println("\n\n------ 10x10 Graph ------");
+        Graph graph = new Graph(15);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
@@ -46,7 +46,7 @@ public class GraphTest {
 
     @org.junit.Test
     public void graph20x20() {
-        System.out.println("\n\n------ 20x20 Graph ------");
+        System.out.println("\n\n------ 16x16 Graph ------");
         Graph graph = new Graph(20);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
@@ -54,9 +54,27 @@ public class GraphTest {
     }
 
     @org.junit.Test
+    public void graph25x25() {
+        System.out.println("\n\n------ 20x20 Graph ------");
+        Graph graph = new Graph(25);
+        System.out.print(graph.printMaze());
+        System.out.print(graph.printBFS());
+        System.out.print(graph.printDFS());
+    }
+
+    @org.junit.Test
     public void graph30x30() {
+        System.out.println("\n\n------ 25x25 Graph ------");
+        Graph graph = new Graph(30);
+        System.out.print(graph.printMaze());
+        System.out.print(graph.printBFS());
+        System.out.print(graph.printDFS());
+    }
+
+    @org.junit.Test
+    public void graph35x35() {
         System.out.println("\n\n------ 30x30 Graph ------");
-        Graph graph = new Graph(20);
+        Graph graph = new Graph(35);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
@@ -65,7 +83,16 @@ public class GraphTest {
     @org.junit.Test
     public void graph40x40() {
         System.out.println("\n\n------ 40x40 Graph ------");
-        Graph graph = new Graph(20);
+        Graph graph = new Graph(40);
+        System.out.print(graph.printMaze());
+        System.out.print(graph.printBFS());
+        System.out.print(graph.printDFS());
+    }
+
+    @org.junit.Test
+    public void graph45x45() {
+        System.out.println("\n\n------ 50x50 Graph ------");
+        Graph graph = new Graph(45);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
@@ -73,10 +100,10 @@ public class GraphTest {
 
     @org.junit.Test
     public void graph50x50() {
-        System.out.println("\n\n------ 50x50 Graph ------");
-        Graph graph = new Graph(20);
+        System.out.println("\n\n------ 100x100 Graph ------");
+        Graph graph = new Graph(50);
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
-    } */
+    }
 }
