3e4820cc3b3da6a100e2f24205e7f6f5b5dbbcba
Keshav Narula <narulakeshav13@gmail.com>
2016-12-04 21:07:12 -0800

    Comments

--- src/sjsu/narula/cs146/project3/GraphTest.java
+++ src/sjsu/narula/cs146/project3/GraphTest.java
@@ -3,10 +3,11 @@ package sjsu.narula.cs146.project3;
 import static org.junit.Assert.*;
 
 /**
- * Created by narulakeshav on 12/4/16.
+ * Tests the Maze
  */
 public class GraphTest {
 
+    // 1x1 Graph
     @org.junit.Test
     public void graph1x1() {
         System.out.println("\n\n------- 4x4 Graph -------");
@@ -14,8 +15,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 5x5 Maze
     @org.junit.Test
     public void graph5x5() {
         System.out.println("\n\n------- 4x4 Graph -------");
@@ -23,9 +26,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
-
+    // 10x10 Maze
     @org.junit.Test
     public void graph10x10() {
         System.out.println("\n\n------- 8x8 Graph -------");
@@ -35,6 +39,7 @@ public class GraphTest {
         System.out.print(graph.printDFS());
     }
 
+    // 15x15 Maze
     @org.junit.Test
     public void graph15x15() {
         System.out.println("\n\n------ 10x10 Graph ------");
@@ -42,8 +47,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 20x20 Maze
     @org.junit.Test
     public void graph20x20() {
         System.out.println("\n\n------ 16x16 Graph ------");
@@ -51,8 +58,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 25x25 Maze
     @org.junit.Test
     public void graph25x25() {
         System.out.println("\n\n------ 20x20 Graph ------");
@@ -60,8 +69,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 30x30 Maze
     @org.junit.Test
     public void graph30x30() {
         System.out.println("\n\n------ 25x25 Graph ------");
@@ -69,8 +80,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 35x35 Maze
     @org.junit.Test
     public void graph35x35() {
         System.out.println("\n\n------ 30x30 Graph ------");
@@ -78,8 +91,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 40x40 Maze
     @org.junit.Test
     public void graph40x40() {
         System.out.println("\n\n------ 40x40 Graph ------");
@@ -87,8 +102,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 45x45 Maze
     @org.junit.Test
     public void graph45x45() {
         System.out.println("\n\n------ 50x50 Graph ------");
@@ -96,8 +113,10 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 
+    // 50x50 Maze
     @org.junit.Test
     public void graph50x50() {
         System.out.println("\n\n------ 100x100 Graph ------");
@@ -105,5 +124,6 @@ public class GraphTest {
         System.out.print(graph.printMaze());
         System.out.print(graph.printBFS());
         System.out.print(graph.printDFS());
+        assertEquals(graph.printBFS(), graph.printDFS());
     }
 }
